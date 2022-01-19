87abbb073d9967ef778f487b3cbb2986a7786f76
vladimir-code47 <85311460+vladimir-code47@users.noreply.github.com>
2022-01-19 22:39:58 +0100

    Update site.config.js
    Changed rootPage ID

--- site.config.js
+++ site.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
-
+  rootNotionPageId: 'ebf508a24d50454e90c74c2e577a39db',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
